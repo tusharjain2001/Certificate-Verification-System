29ff00801aea2b50265902e834156c5f1c69c439
Tushar Jain <tusharjainqwerty@gmail.com>
2024-10-05 19:58:34 +0530

    done

--- server/configuration/db.js
+++ server/configuration/db.js
@@ -1,17 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect(process.env.DB_URI);
 console.log(process.env.DB_URI);
 
-mongoose.connection.on("connected",() => {
-    console.log("connected to mongoDB");
-})
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongoDB");
+});
 
-mongoose.connection.on("error",(err) => {
-    console.log(`MongoDB connection error: ${err}`)
-})
+mongoose.connection.on("error", (err) => {
+  console.log(`MongoDB connection error: ${err}`);
+});
 
 module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const connectDatabase = require("../server/configuration/db");
-
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 
-
-
 const port = process.env.PORT || 8080;
-app.listen(port,() => console.log(`Listening on ${port}...`));
-
-
+app.listen(port, () => console.log(`Listening on ${port}...`));
