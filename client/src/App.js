b11b78d259a6ce889e12a9ea91c9e72d21616c79
Tushar Jain <tusharjainqwerty@gmail.com>
2024-10-06 01:51:05 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-
-import './App.css';
-import Header from './Header/Header';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Header/Header";
+import StudentLogin from "./Login/StudentLogin";
+import ManagerLogin from "./Login/ManagerLogin";
+import StudentRegister from "./SignUp/StudentRegister";
+import ManagerRegister from "./SignUp/ManagerRegister";
+import ManagerDash from "./Dashboard/ManagerDash";
+import StudentDash from "./Dashboard/StudentDash";
 
 function App() {
   return (
-    <div className="text-9xl">
-      <Header/>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/manager/login" element={<ManagerLogin />} />
+        <Route path="/student/login" element={<StudentLogin />} />
+        <Route path="/manager/register" element={<ManagerRegister />} />
+        <Route path="/student/register" element={<StudentRegister />} />
+        <Route path="/manager/dash" element={<ManagerDash />} />
+        <Route path="/student/dash" element={<StudentDash />} />
+        
+        
+        
+      </Routes>
     </div>
   );
 }
